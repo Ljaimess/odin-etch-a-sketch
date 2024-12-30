c3c7d5ae4b676ad7d6c8c964fcc2b97a804e9da1
Laurentino Jaimes <Ljaimes92@gmail.com>
2024-12-30 16:52:08 -0500

    got the grid made using two for loops (prob a better way to do this)

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+const containerDiv = document.querySelector("#container");
+
+const makeRow = () => {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("divRow");
+
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("drawBox");
+        rowDiv.appendChild(div);
+    }
+
+    return rowDiv;
+};
+
+const makeGrid = () => {
+    for (let i = 0; i < 16; i++) {
+        const rowDiv = makeRow();
+        containerDiv.appendChild(rowDiv);
+    }
+};
+
+makeGrid();
+
